faa893c711c3c99af348af2bc461c97fb2b32208
Jzdoot <jacobsamurin@gmail.com>
2022-12-12 09:46:43 -0500

    we did a thing

--- src/Director.java
+++ src/Director.java
@@ -1,10 +1,37 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Director extends Officer{
 	public void stats (){
 
 	}
-	public void updateBand(File f){
+	public void updateBand(File f, boolean topLine) throws FileNotFoundException{
+		Scanner s = new Scanner(f);
+		s.useDelimiter(",");
+		int count=0;
+		if(topLine)
+			s.nextLine();
+		Map<String,String[]> bl=new HashMap<String,String[]>();
+		while(s.hasNext()){
+			System.out.println(s.next());
+			String[] nameArr = new String[2];
+			String item = s.next();
+			switch(count%3){
+				case 0:
 
+					break;
+				case 1:
+					nameArr[0]=item;
+					break;
+				case 2:
+					nameArr[1]=item;
+					break;
+			}
+			count++;
+		}
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-public class Main{
-	public static void main(String[] args){
+import java.io.File;
+import java.io.FileNotFoundException;
 
+public class Main{
+	public static void main(String[] args) throws FileNotFoundException{
+		Director d = new Director();
+		d.updateBand(new File("testStuff/UAMB Attendance 2022-23 - Week 2 - 8_22-8_26.csv"), true);
 	}
 }
